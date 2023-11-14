c91bf6e3f5fc4645169ae5c2208cb5b7a66c7ff5
Elijah Tran Inofinada <elijahboy808@yahoo.com>
2023-11-14 13:13:40 -1000

    y

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
 import { Roles } from 'meteor/alanning:roles';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
+import { BoxArrowRight, PersonFill, PersonPlusFill, BellFill } from 'react-bootstrap-icons';
 
 const NavBar = () => {
     // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
@@ -17,6 +17,7 @@ const NavBar = () => {
             <Container>
                 <Nav className="justify-content-start">
                     <Navbar.Brand as={NavLink} to="/" >
+                       <img src="/images/uhmainlogo.png" className="custom-img" alt="UHM main logo"/>
                         <h2>MARKETPLACE</h2>
                     </Navbar.Brand>
                 </Nav>
@@ -24,8 +25,9 @@ const NavBar = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto justify-content-center">
                         {currentUser ? ([
-                            <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Add Stuff</Nav.Link>,
-                            <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">List Stuff</Nav.Link>,
+                            <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Home</Nav.Link>,
+                            <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">YourListing's</Nav.Link>,
+                            <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">Sell</Nav.Link>,
                         ]) : ''}
                         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
                             <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>
