a42fad4910ba9a0768c5e2f39bde044ffdd2ef9d
Brandon Underwood <103099466+branndonU@users.noreply.github.com>
2023-11-18 14:25:52 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 
-
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
