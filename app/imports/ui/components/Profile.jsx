80e5a886017057621ad3957b575adca20f5a781f
Elijah Tran Inofinada <elijahboy808@yahoo.com>
2023-12-10 19:12:16 -1000

    yep

--- app/imports/ui/components/Profile.jsx
+++ app/imports/ui/components/Profile.jsx
@@ -1,34 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image } from 'react-bootstrap';
+import { Container, Image, Button, Modal, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-// eslint-disable-next-line react/prop-types
-const Profile = ({ profile }) => (
-  <Card className="h-100">
-    <Card.Header>
-      <Image src={profile.image} width={75} />
-      <Card.Title>{profile.firstName} {profile.lastName}</Card.Title>
-      <Card.Subtitle>{profile.address}</Card.Subtitle>
-    </Card.Header>
-    <Card.Body>
-      <Card.Text>{profile.description}</Card.Text>
-    </Card.Body>
-    <Link to={`/edit/${profile._id}`}>Edit</Link>
-  </Card>
+// Header component
+const Header = () => (
+    <div className="text-light text-center mb-4">
+        <h5>Click on your profile to enlarge the contents</h5>
+    </div>
 );
 
-// Require a document to be passed to this component.
+const ProfileContentCard = ({ profile, onCardClick }) => (
+    <Card className="bg-light p-4 text-center" onClick={onCardClick}>
+        <Card.Img src={profile.image} width={100} className="rounded-circle mb-3" />
+        <Card.Title>{profile.firstName} {profile.lastName}</Card.Title>
+        <Card.Subtitle className="text-muted">{profile.address}</Card.Subtitle>
+        <Card.Text>{profile.description}</Card.Text>
+    </Card>
+);
+
+ProfileContentCard.propTypes = {
+    profile: PropTypes.shape({
+        firstName: PropTypes.string,
+        lastName: PropTypes.string,
+        address: PropTypes.string,
+        image: PropTypes.string,
+        description: PropTypes.string,
+        _id: PropTypes.string,
+    }).isRequired,
+    onCardClick: PropTypes.func.isRequired,
+};
+
+const Profile = ({ profile }) => {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClose = () => setShowModal(false);
+    const handleShow = () => setShowModal(true);
+
+    const handleCardClick = () => {
+        // Open the modal when the card is clicked
+        handleShow();
+    };
+
+    return (
+        <Container className="profile-container d-flex align-items-center justify-content-center flex-column" style={{ minHeight: '100vh', backgroundImage: `url(${profile.backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            {showModal && (
+                // Modal is open
+                <Modal show={showModal} onHide={handleClose} centered>
+                    <Modal.Body className="text-center bg-light p-5">
+                        <Image src={profile.image} width={200} className="mb-3 rounded-circle" />
+                        <h2>{profile.firstName} {profile.lastName}</h2>
+                        <p className="text-muted">{profile.address}</p>
+                        <p>{profile.description}</p>
+                        <Link to={`/edit/${profile._id}`}>
+                            <Button variant="primary">Edit Profile</Button>
+                        </Link>
+                        <Button variant="secondary" onClick={handleClose} className="ml-2">
+                            Close
+                        </Button>
+                    </Modal.Body>
+                </Modal>
+            )}
+
+            {/* Header component */}
+            <Header />
+
+            {/* Display the card-like component with a solid background color */}
+            <div style={{ backgroundColor: '#fff', borderRadius: '10px', padding: '20px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+                <ProfileContentCard profile={profile} onCardClick={handleCardClick} />
+            </div>
+        </Container>
+    );
+};
+
 Profile.propTypes = {
-  profile: PropTypes.shape({
-    firstName: PropTypes.string,
-    lastName: PropTypes.string,
-    address: PropTypes.string,
-    image: PropTypes.string,
-    description: PropTypes.string,
-    _id: PropTypes.string,
-  }).isRequired,
+    profile: PropTypes.shape({
+        firstName: PropTypes.string,
+        lastName: PropTypes.string,
+        address: PropTypes.string,
+        image: PropTypes.string,
+        description: PropTypes.string,
+        backgroundImage: PropTypes.string,
+        _id: PropTypes.string,
+    }).isRequired,
 };
 
 export default Profile;
--- app/imports/ui/pages/SignUp.jsx
+++ app/imports/ui/pages/SignUp.jsx
@@ -2,47 +2,59 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link, Navigate } from 'react-router-dom';
 import { Accounts } from 'meteor/accounts-base';
-import {Alert, Button, Card, Col, Container, Image, Row} from 'react-bootstrap';
+import { Alert, Card, Col, Container, Row, Image, Button } from 'react-bootstrap';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-bootstrap5';
-import UploadFile from "../components/UploadFile";
+import CloudinaryUpload from '../services/CloudinaryUpload'; // Import your CloudinaryUpload service
+import UploadFile from '../components/UploadFile'; // Import your UploadFile component
 
 const SignUp = ({ location }) => {
   const [error, setError] = useState('');
   const [redirectToReferer, setRedirectToRef] = useState(false);
+  const [pictureSelected, setPictureSelected] = useState(null);
 
   const schema = new SimpleSchema({
     email: String,
     password: String,
-    fullName: String,
+    name: String,
     address: String,
-    description: String,
-    Profile Picture: {
-      type: Array,
-      optional: true,
-    },
-    'images.$': {
-      type: String,
+    bio: String,
+    picture: {
+      type: String, // Change to String for a single image
       optional: true,
     },
   });
   const bridge = new SimpleSchema2Bridge(schema);
 
-  const submit = (doc) => {
-    const { email, password } = doc;
+  const submit = async (doc) => {
+    const { email, password, name, address, bio } = doc;
+
     if (!email.endsWith('@hawaii.edu')) {
       setError('Only emails ending with @hawaii.edu are allowed to register.');
     } else {
-      Accounts.createUser({ email, username: email, password }, (err) => {
-        if (err) {
-          setError(err.reason);
-        } else {
-          setError('');
-          setRedirectToRef(true);
-        }
-      });
+      // Additional logic for image upload if a picture is selected
+      let uploadedPicture = '';
+      if (pictureSelected) {
+        uploadedPicture = await CloudinaryUpload.postImage(pictureSelected);
+      }
+
+      Accounts.createUser(
+          { email, username: email, password, profile: { name, address, bio, picture: uploadedPicture } },
+          (err) => {
+            if (err) {
+              setError(err.reason);
+            } else {
+              setError('');
+              setRedirectToRef(true);
+            }
+          }
+      );
     }
+  };
+
+  const handleImagePreview = (selectedFile) => {
+    setPictureSelected(selectedFile);
   };
 
   const { from } = location?.state || { from: { pathname: '/add' } };
@@ -51,38 +63,47 @@ const SignUp = ({ location }) => {
   }
 
   return (
-    <Container id="signup-page" className="py-3">
-      <Row className="justify-content-center">
-        <Col xs={5} >
-          <h2 className="text-center">Register your account</h2>
-          <AutoForm schema={bridge} onSubmit={data => submit(data)}>
-            <Card>
-              <Card.Body>
-                <TextField name="fullName" placeholder="Full name" />
-                <TextField name="images" placeholder="Upload a profile picture" />
-                <TextField name="email" placeholder="Enter a UH email address" />
-                <TextField name="address" placeholder="Address" />
-                <LongTextField name="description" placeholder="Description" />
-                <TextField name="password" placeholder="Password" type="password" />
+      <Container id="signup-page" className="py-3">
+        <Row className="justify-content-center">
+          <Col xs={5}>
+            <h2 className="text-center">Register your account</h2>
+            <AutoForm schema={bridge} onSubmit={(data) => submit(data)}>
+              <Card>
+                <Card.Body>
+                  {/* Image upload component */}
+                  <UploadFile handleImagePreview={handleImagePreview} />
 
+                  {pictureSelected && (
+                      <div>
+                        <Image src={URL.createObjectURL(pictureSelected)} thumbnail />
+                        <Button onClick={() => setPictureSelected(null)}>Delete </Button>
+                      </div>
+                  )}
+                  
 
-                <ErrorsField />
-                <SubmitField />
-              </Card.Body>
-            </Card>
-          </AutoForm>
-          <Alert variant="light">
-            Already have an account? Login <Link to="/signin">here</Link>
-          </Alert>
-          {error && (
-            <Alert variant="danger">
-              <Alert.Heading>Registration was not successful</Alert.Heading>
-              {error}
+                  <TextField name="name" placeholder="Full name" />
+                  <TextField name="email" placeholder="Enter a UH email address" />
+                  <TextField name="address" placeholder="Address" />
+                  <LongTextField name="bio" placeholder="Enter a description" />
+                  <TextField name="password" placeholder="Password" type="password" />
+
+                  <ErrorsField />
+                  <SubmitField />
+                </Card.Body>
+              </Card>
+            </AutoForm>
+            <Alert variant="light">
+              Already have an account? Login <Link to="/signin">here</Link>
             </Alert>
-          )}
-        </Col>
-      </Row>
-    </Container>
+            {error && (
+                <Alert variant="danger">
+                  <Alert.Heading>Registration was not successful</Alert.Heading>
+                  {error}
+                </Alert>
+            )}
+          </Col>
+        </Row>
+      </Container>
   );
 };
 
